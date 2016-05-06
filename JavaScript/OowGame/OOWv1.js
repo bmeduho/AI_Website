de05c42b90f6f1e6187837fa827fdf6b01cb9d4c
bmeduho <bmeduho@users.noreply.github.com>
2016-05-06 10:02:30 -0400

    Update OOWv1.js

--- JavaScript/OowGame/OOWv1.js
+++ JavaScript/OowGame/OOWv1.js
@@ -1,1 +1,21 @@
+function init () {
+  
+}
 
+function update () {
+  
+}
+
+function render () {
+  
+}
+
+function main () {
+	
+	update();
+	render();
+	requestAnimationFrame(main);
+}
+
+init();
+main();
